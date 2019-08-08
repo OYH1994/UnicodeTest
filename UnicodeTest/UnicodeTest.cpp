50deba7f71a832b5289f8aa9a868e16c3040bc69
OYH1994 <1317631179@qq.com>
2019-08-08 17:36:57 +0800

    first commit

---
+++ UnicodeTest/UnicodeTest.cpp
@@ -0,0 +1,142 @@
+#include "stdafx.h"
+#include <iostream>
+#include <WCHAR.h>
+#include <STRING.h>
+#include <windows.h>
+#include <wchar.h>
+using namespace std;
+void f_test1()
+{
+	int iLength = 0;
+	wchar_t c = 'A';
+	wchar_t *p = L"Hello!";
+	static wchar_t a[] = L"Hello!";
+	cout << "c:" << sizeof(c) << "   a:" << sizeof(a) << endl;
+
+	char *pc = "Hello!";
+	iLength = strlen(pc);
+	cout << "iLength:" << iLength << endl;
+
+	iLength = wcslen(p);
+	cout << "iLength:" << iLength << endl;
+}
+
+wstring ANSIToUnicode(const string& str)
+{
+	int  len = 0;
+	len = str.length();
+	int  unicodeLen = MultiByteToWideChar(CP_ACP,
+		0,
+		str.c_str(),
+		-1,
+		NULL,
+		0);
+	wchar_t *  pUnicode;
+	pUnicode = new  wchar_t[unicodeLen + 1];
+	memset(pUnicode, 0, (unicodeLen + 1)*sizeof(wchar_t));
+	MultiByteToWideChar(CP_ACP,
+		0,
+		str.c_str(),
+		-1,
+		(LPWSTR)pUnicode,
+		unicodeLen);
+	wstring  rt;
+	rt = (wchar_t*)pUnicode;
+	delete  pUnicode;
+
+	return  rt;
+}
+
+string UnicodeToANSI(const wstring& str)
+{
+	char*     pElementText;
+	int    iTextLen;
+	// wide char to multi char
+	iTextLen = WideCharToMultiByte(CP_ACP,
+		0,
+		str.c_str(),
+		-1,
+		NULL,
+		0,
+		NULL,
+		NULL);
+	pElementText = new char[iTextLen + 1];
+	memset((void*)pElementText, 0, sizeof(char) * (iTextLen + 1));
+	WideCharToMultiByte(CP_ACP,
+		0,
+		str.c_str(),
+		-1,
+		pElementText,
+		iTextLen,
+		NULL,
+		NULL);
+	string strText;
+	strText = pElementText;
+	delete[] pElementText;
+	return strText;
+}
+
+string UnicodeToUTF8(const wstring& str)
+{
+	char*     pElementText;
+	int    iTextLen;
+	// wide char to multi char
+	iTextLen = WideCharToMultiByte(CP_UTF8,
+		0,
+		str.c_str(),
+		-1,
+		NULL,
+		0,
+		NULL,
+		NULL);
+	pElementText = new char[iTextLen + 1];
+	memset((void*)pElementText, 0, sizeof(char) * (iTextLen + 1));
+	::WideCharToMultiByte(CP_UTF8,
+		0,
+		str.c_str(),
+		-1,
+		pElementText,
+		iTextLen,
+		NULL,
+		NULL);
+	string strText;
+	strText = pElementText;
+	delete[] pElementText;
+	return strText;
+}
+
+wstring UTF8ToUnicode(const string& str)
+{
+	int  len = 0;
+	len = str.length();
+	int  unicodeLen = ::MultiByteToWideChar(CP_UTF8,
+		0,
+		str.c_str(),
+		-1,
+		NULL,
+		0);
+	wchar_t *  pUnicode;
+	pUnicode = new  wchar_t[unicodeLen + 1];
+	memset(pUnicode, 0, (unicodeLen + 1)*sizeof(wchar_t));
+	::MultiByteToWideChar(CP_UTF8,
+		0,
+		str.c_str(),
+		-1,
+		(LPWSTR)pUnicode,
+		unicodeLen);
+	wstring  rt;
+	rt = (wchar_t*)pUnicode;
+	delete  pUnicode;
+
+	return  rt;
+}
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	f_test1();
+	wstring wstr = ANSIToUnicode("ÎÒ°®c++");
+	string str = UnicodeToANSI(wstr);
+	system("pause");
+	return 0;
+}
+
